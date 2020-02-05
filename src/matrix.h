d2ab0005b33b364d53846ae1721e705f584bfd75
kyawakyawa <kyawashell@gmail.com>
2020-02-05 12:36:02 +0900

    matrix

---
+++ src/matrix.h
@@ -0,0 +1,20 @@
+#ifndef PBRLAB_MATRIX_H_
+#define PBRLAB_MATRIX_H_
+
+namespace pbrlab {
+
+class Matrix {
+public:
+  Matrix();
+  ~Matrix();
+
+  static void Print(float m[4][4]);
+  static void LookAt(float m[4][4], float eye[3], float lookat[3], float up[3]);
+  static void Inverse(float m[4][4]);
+  static void Mult(float dst[4][4], float m0[4][4], float m1[4][4]);
+  static void MultV(float dst[3], float m[4][4], float v[3]);
+};
+
+}  // namespace pbrlab
+
+#endif  // PBRLAB_MATRIX_H_
