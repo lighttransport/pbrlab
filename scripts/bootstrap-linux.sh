46baa5d7c00a55e9ba5a675d832c7301685f920f
kyawakyawa <kyawashell@gmail.com>
2020-05-26 10:36:04 +0900

    fix relwithdebinfo

--- scripts/bootstrap-linux.sh
+++ scripts/bootstrap-linux.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-
-rm -rf build
-mkdir build
-cd build
+rm -rf cmake-build-debug
+rm -rf cmake-build-relwithdebinfo
+rm -rf cmake-build-release
 
 CXX=clang++ CC=clang cmake \
+  -Bcmake-build-debug -H. \
+  -DPBRLAB_USE_CCACHE=On \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+  -DPBRLAB_WITH_EMBREE=On \
+  ..
+
+mv cmake-build-debug/compile_commands.json .
+
+CXX=clang++ CC=clang cmake \
+  -Bcmake-build-relwithdebinfo -H. \
   -DPBRLAB_USE_CCACHE=On \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
   -DPBRLAB_WITH_EMBREE=On \
   ..
 
-mv compile_commands.json ..
-cd ..
+CXX=clang++ CC=clang cmake \
+  -Bcmake-build-release -H. \
+  -DPBRLAB_USE_CCACHE=On \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+  -DPBRLAB_WITH_EMBREE=On \
+  ..
